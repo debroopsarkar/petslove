d97cade8a8a09437cd2d06a5e9981bf37ce420f6
Debroop Sarkar <72680117+debroopsarkar@users.noreply.github.com>
2024-04-12 19:14:25 +0530

    Update involve.php

--- involve.php
+++ involve.php
@@ -1,9 +1,9 @@
 <?php
 // Database connection parameters
-$hostname = "localhost";
-$username = "pma";
-$password = "";
-$database = "pet_registration";
+$hostname = "sql6.freesqldatabase.com";
+$username = "sql6698495";
+$password = "Q8Cm2zjE3a";
+$database = "sql6698495";
 
 // Create connection
 $conn = new mysqli($hostname, $username, $password, $database);
