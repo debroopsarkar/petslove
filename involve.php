9d385dac17271b06c0996e5b8d5149e826361501
Debroop Sarkar <72680117+debroopsarkar@users.noreply.github.com>
2024-04-13 14:47:50 +0530

    Update involve.php

--- involve.php
+++ involve.php
@@ -11,22 +11,19 @@ $conn .= ";port=" . $fields["port"];;
 $conn .= ";dbname=defaultdb";
 $conn .= ";sslmode=verify-ca;sslrootcert=ca.pem";
 
-$first_name = $_POST['first_name'];
-$last_name = $_POST['last_name'];
-$email = $_POST['email'];
-$message = $_POST['message'];
+
 // Check connection
 try {
     $db = new PDO($conn, $fields["user"], $fields["pass"]);
 
     $stmt = $db->prepare("INSERT INTO register (first_name, last_name, email, message) VALUES (?, ?, ?, ?)");
 
-    $owner_name = filter_var($_POST['first_name'], FILTER_SANITIZE_STRING);
-    $pet_name = filter_var($_POST['last_name'], FILTER_SANITIZE_STRING);
-    $pet_type = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+    $first-name = filter_var($_POST['first-name'], FILTER_SANITIZE_STRING);
+    $last-name = filter_var($_POST['last-name'], FILTER_SANITIZE_STRING);
+    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
     $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-    $execval = $stmt->execute([$owner_name, $pet_name, $pet_type, $pet_age, $mobile, $days, $message]);
+    $execval = $stmt->execute([$first-name, $last-name, $email, $pet_age, $message]);
 
     if ($execval) {
       echo "Registration successful...";
